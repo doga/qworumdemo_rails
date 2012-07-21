e5be190b4dc8dda948c89f9710fcbfb72c8b31fd
Doga Armangil <armangild@yahoo.com>
2012-07-21 19:05:48 +0200

    fix redirect bug

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,19 +1,17 @@
 class SiteController < ApplicationController
   before_filter :parse_xml_post, only: :receive_call_result
 
+  def index
+  end
+  
   # Generate Qworum message for calling the service defined above
   def call_service
     @name = 'Dave'
-    render template: "#{params[:controller]}/#{params[:action]}", formats: [:xml], handlers: [:builder]
+    render_xml
   end
 
   # Receive the result of service call
   def receive_call_result
     @sentence = @xml.elements['/sentence'].text
-  end
-  
-  # ignore this (not part of demo)
-  def test_select
-    render template: "#{params[:controller]}/#{params[:action]}", formats: [:xml], handlers: [:erb]
   end
 end
