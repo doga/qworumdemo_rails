a56488bc830e2963b0108b7bf0dd7d6fe6aa3e05
Doga Armangil <armangild@yahoo.com>
2012-07-21 19:18:10 +0200

    fix redirect bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 QworumHerokuAddonTestHerokuappCom::Application.routes.draw do
-  match '', controller: :site, action: :index
+  match '' => redirect('/site')
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
