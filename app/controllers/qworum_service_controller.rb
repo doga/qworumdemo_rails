79a96d1a39f7b3d0be9a91632f18019c233a857c
Doga Armangil <armangild@yahoo.com>
2012-07-21 17:32:48 +0200

    add comments to qworum message

--- app/controllers/qworum_service_controller.rb
+++ app/controllers/qworum_service_controller.rb
@@ -1,17 +1,22 @@
 class QworumServiceController < ApplicationController
   before_filter :parse_xml_post, except: :result
 
-  # Read name argument, and return a sentence.
+  # Phase 1:
+  # Receive call argument, compute result
   def index
     name = @xml.elements['/name'].text
     @sentence = "Hello #{name} !"
     render_xml
   end
   
+  # Phase 2:
+  # Show result to end-user
   def show
     @sentence = @xml.elements['/sentence'].text
   end
   
+  # Phase 3:
+  # Return result
   def result
     render_xml
   end
--- app/views/site/call_service.xml.builder
+++ app/views/site/call_service.xml.builder
@@ -1,3 +1,8 @@
+xml.comment!("\n"+
+  "  QWORUM SUPPORT IS MISSING IN YOUR BROWSER:\n"+
+  "  This site only works correctly with Qworum enabled browsers. \n"+
+  "  Please visit  http://www.qworum.com/products\n"
+)
 xml.qrm :goto, href: url_for(action: :receive_call_result), 'xmlns:qrm' => 'http://qworum.net/' do
   xml.qrm :call, href: url_for(controller: :qworum_service) do
     xml.name @name
